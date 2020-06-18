0be893f02cde48deed7a79bf70618fa625fd9c3e
Gaurav Sharma <66160533+gaurav0o7@users.noreply.github.com>
2020-06-19 01:22:20 +0530

    commit

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,11 +4,11 @@
 
 # Step 1:
 # This is your Docker ID/path
-dockerpath=manishbisht/development
+dockerpath=gaurav0o7/development
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run development --image=manishbisht/development --port=80
+kubectl run development --image=gaurav0o7/development --port=80
 
 # Step 3:
 # List kubernetes pods
